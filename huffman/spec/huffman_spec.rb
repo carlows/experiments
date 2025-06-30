aebec69a20e96464e0be63d7ade51ad613d9e2dc
Carlos Martinez <carlos.martinez@medwing.com>
2025-06-30 19:20:19 +0200

    Cleanup

--- huffman/spec/huffman_spec.rb
+++ huffman/spec/huffman_spec.rb
@@ -5,9 +5,9 @@ describe "Huffman" do
   context 'encode' do
     it 'generate an output file with a header that includes the prefix table' do
       root_dir = File.expand_path("..", __dir__)
-      FileUtils.rm_f("#{root_dir}/output")
-      output_file = "#{root_dir}/output"
-      Huffman.new.encode("#{root_dir}/lesmiserables.txt", output_file)
+      FileUtils.rm_f("#{root_dir}/fixtures/output")
+      output_file = "#{root_dir}/fixtures/output"
+      Huffman.new.encode("#{root_dir}/fixtures/lesmiserables.txt", output_file)
 
       File.open(output_file, 'rb') do |file| 
         json_length = file.read(4).unpack('V').first
@@ -19,9 +19,9 @@ describe "Huffman" do
 
     it 'stores the padding length' do
       root_dir = File.expand_path("..", __dir__)
-      FileUtils.rm_f("#{root_dir}/output")
-      output_file = "#{root_dir}/output"
-      Huffman.new.encode("#{root_dir}/sample.txt", output_file)
+      FileUtils.rm_f("#{root_dir}/fixtures/output")
+      output_file = "#{root_dir}/fixtures/output"
+      Huffman.new.encode("#{root_dir}/fixtures/sample.txt", output_file)
 
       File.open(output_file, 'rb') do |file| 
         length = file.read(4).unpack('V').first # skip the json table length
@@ -36,9 +36,9 @@ describe "Huffman" do
   context 'decode' do
     it 'decodes the encoded file' do
       root_dir = File.expand_path("..", __dir__)
-      FileUtils.rm_f("#{root_dir}/output")
-      output_file = "#{root_dir}/output"
-      Huffman.new.encode("#{root_dir}/test_short.txt", output_file)
+      FileUtils.rm_f("#{root_dir}/fixtures/output")
+      output_file = "#{root_dir}/fixtures/output"
+      Huffman.new.encode("#{root_dir}/fixtures/test_short.txt", output_file)
       table = Huffman.new.decode(output_file, "#{root_dir}/test_short_decoded.txt")
 
       expect(File.read("#{root_dir}/test_short_decoded.txt")).to eq("hi ")
