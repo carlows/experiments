70456d6684b244cab15061b4e34459100bef6c2e
Carlos Martinez <carlos.martinez@medwing.com>
2025-09-07 00:41:54 +0200

    Solve part 2 day 2

---
+++ advent-of-code/05_part2.rb
@@ -0,0 +1,92 @@
+def calculate(file)
+  rules, updates = process_input(file)
+  map = build_map(rules)
+
+  middle_nums = updates.filter_map do |update|
+    numbers = update.split(',').map(&:to_i)
+    raise StandardError.new("No even rows accepted") if numbers.size % 2 == 0
+
+    missordered = fix_missordering(numbers, map)
+    next nil unless missordered
+
+    numbers[numbers.size / 2]
+  end
+
+  middle_nums.sum
+end
+
+def fix_missordering(numbers, rules)
+  missordered = false
+  numbers.each_with_index do |num, idx|
+    remaining = numbers[idx + 1..]
+    number_to_swap = rules[num].intersection(remaining)
+    next if number_to_swap.empty?
+
+    missordered = true
+    # do the swapping
+    idx_2 = numbers.index(number_to_swap[0])
+    numbers[idx] = numbers[idx_2]
+    numbers[idx_2] = num
+    # after a swapping we might still be missordered
+    fix_missordering(numbers, rules)
+  end
+  missordered
+end
+
+def process_input(file)
+  rules, updates = file.split("\n\n")
+  rules = rules.split("\n")
+  updates = updates.split("\n")
+  [rules, updates]
+end
+
+def build_map(rules)
+  map = Hash.new { |hsh, key| hsh[key] = [] }
+  rules.each_with_object(map) do |rule, obj|
+    x, y = rule.split("|")
+    obj[y.to_i] << x.to_i
+  end
+  map
+end
+
+require_relative './assert'
+
+extend SuperDuperAssertions
+
+
+page_example = <<~TEXT
+  47|53
+  97|13
+  97|61
+  97|47
+  75|29
+  61|13
+  75|53
+  29|13
+  97|29
+  53|29
+  61|53
+  97|53
+  61|29
+  47|13
+  75|47
+  97|75
+  47|61
+  75|61
+  47|29
+  75|13
+  53|13
+
+  75,47,61,53,29
+  97,61,53,29,13
+  75,29,13
+  75,97,47,61,53
+  61,13,29
+  97,13,75,29,47
+TEXT
+
+assert("More contrived example", calculate(page_example), 123)
+
+real_input = File.read('./input05.txt')
+
+assert("Gives teh result!!", calculate(real_input), 5346)
