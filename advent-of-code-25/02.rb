a69c831737b63a6e2b2ac18a4c784bda30555987
Carlos Martinez <carlos.martinez@medwing.com>
2025-12-02 18:00:36 +0100

    solve part 2

--- advent-of-code-25/02.rb
+++ advent-of-code-25/02.rb
@@ -1,4 +1,5 @@
 require_relative './assert'
+require 'benchmark'
 
 def invalid_ids(input)
   ranges = parse(input)
@@ -6,8 +7,10 @@ def invalid_ids(input)
   ranges.reduce(0) do |sum, range|
     range_sum = range.filter do |i|
       num = i.to_s
-      half_of_num = num[0...num.size / 2]
-      num.size.even? && num.match?(/(#{half_of_num}){2}/)
+      (0..num.size - 1).any? do |substr| 
+        match = num.match(/(#{num[0..substr]}){2,}/)
+        match.to_a.any? && match[0] == num
+      end
     end.flatten.sum
     sum += range_sum
   end
@@ -23,10 +26,12 @@ end
 
 extend SuperDuperAssertions
 
-assert('works with simple double digit ranges', invalid_ids("11-22,95-115"), 132)
+assert('works with simple double digit ranges', invalid_ids("11-22,95-115"), 243)
 
-assert('works with the sample input', invalid_ids("11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"), 1227775554)
+assert('works with the sample input', invalid_ids("11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"), 4174379265)
 
 real_input = File.read('input02.txt')
 
-assert('works with the real deal', invalid_ids(real_input), 15873079081)
+puts Benchmark.measure {
+  assert('works with the real deal', invalid_ids(real_input), 22617871034)
+}
