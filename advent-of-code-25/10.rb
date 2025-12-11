0fbec7b07feb04281c2e90e85cfc956a33c17cea
Carlos Martinez <carlos.martinez@medwing.com>
2025-12-11 20:23:44 +0100

    solve day 10 part one

---
+++ advent-of-code-25/10.rb
@@ -0,0 +1,66 @@
+def solve_part_one(input)
+  machines = parse(input)
+
+  machines.reduce(0) do |acc, machine|
+    acc + solve_machine(machine)
+  end
+end
+
+def parse(input)
+  lines = input.split("\n")
+  lines.map do |line|
+    parts = line.match(/\[(.+)\] (\(.+\))+/)
+    goal = parts[1].split('').map { |c| c == '#' ? 1 : 0 }.join('').to_i(2)
+    buttons = parts[2].split(' ').map { |b| b.tr('()', '') }
+    buttons = buttons.map { |b| b.split(',').map(&:to_i) }
+    buttons = buttons.map do |b|
+      output = Array.new(parts[1].size) { 0 }
+      b.each do |i|
+        output[i] = 1
+      end
+      output.join('').to_i(2)
+    end
+    [goal, buttons]
+  end
+end
+
+def solve_machine(input)
+  goal, buttons = input
+
+  seen = Set.new
+  next_states = []
+  # presses, state
+  next_states << [0, 0]
+
+  while next_states.any?
+    current_presses, current_state = next_states.shift
+
+    buttons.each do |button|
+      next_state = current_state ^ button
+
+      if next_state == goal
+        return current_presses + 1
+      end
+
+      if !seen.include?(next_state)
+        seen.add(next_state)
+        next_states << [current_presses + 1, next_state]
+      end
+    end
+  end
+end
+
+require_relative './assert'
+
+extend SuperDuperAssertions
+
+input = <<~INPUT
+[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
+INPUT
+
+assert("works with the sample input", solve_part_one(input), 7)
+
+real_input = File.read('input10.txt')
+assert('works with the real input', solve_part_one(real_input), 441)
